a343ef0605548134486ec7b6ec9c621516aeed2d
AVVSSKUM <56311123+AVVSSKUM@users.noreply.github.com>
2022-01-03 20:40:59 +0530

    Update Banknifty.py

--- Banknifty.py
+++ Banknifty.py
@@ -12,12 +12,16 @@ flag = 0
 bot_token = '2128421945:AAHz-s9UwmnCoXP4zbcQjTUm5KSS0EtHzl8'
 bot_chatID = '1142557708'
 symbol = 'BANKNIFTY'
+symbol1 = 'NIFTY'
 def telegram_bot_sendtext(bot_message):
     send_text= 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode = MarkdownV2&text=' +bot_message
     response = requests.get(send_text)
     return response.json()
 while(True):
      curr_price = obj.ltpData('NSE', symbol, 26009)['data']['ltp']
+     curr_price1 = obj.ltpData('NSE', symbol1, 26000)['data']['ltp']
      telegram_bot_sendtext("Banknifty:")
      telegram_bot_sendtext(str(curr_price))
+     telegram_bot_sendtext("Nifty:")
+     telegram_bot_sendtext(str(curr_price1))
      time.sleep(3600)
